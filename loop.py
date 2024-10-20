57f486126ee936d902fd4b7b0326eca46a1c0a4f
hambir2005 <vaishnavihambir3@gmail.com>
2024-10-20 12:04:39 +0530

    first commit

---
+++ loop.py
@@ -0,0 +1,30 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:\Users\Vaishnavi\AppData\Local\Programs\Python\Python312\LOOP.py =====
+enter your starting number3
+enetr your starting number of the range1
+enetr your eneding value21
+3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+3 * 7 = 21
+3 * 8 = 24
+3 * 9 = 27
+3 * 10 = 30
+>>> 
+====== RESTART: C:\Users\Vaishnavi\AppData\Local\Programs\Python\Python312\LOOP.py =====
+enter your number5
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
